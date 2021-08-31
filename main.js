db85718ab3d3310e3447de63da976abcfa7fdbcf
dhiaan <82027019+dhiaan@users.noreply.github.com>
2021-08-31 17:36:10 +0845

    Add files via upload

--- main.js
+++ main.js
@@ -12,6 +12,38 @@ png_quality:90,
 
    Webcam.attach('#camera');
 
+  function take_snapshot()
+  {
+     Webcam.snap(function(data_url) {
+      document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_url+'"/>';
+     });
 
+
+  }
+
+  console.log('ml5 version:', ml5.version);
+
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+
+  function modelLoaded() {
+     console.log('Model Loaded!');
+  }
+
+  function check()
+  {
+ img = document.getElementById('capture_image');
+classifier.classify(img, gotResult);
+  }
+
+  function gotResult(error , results) {
+ if (error) {
+  console.error(error);
+ } else {
+   console.log(results);
+  document.getElementById("object_name").innerHTML = results[0].label;
+  
+ }
+
+  }
 
 
